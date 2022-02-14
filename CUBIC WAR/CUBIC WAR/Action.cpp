06205b528c0e9452fa02aa373ed4831b270bc483
siksaka09 <90751535+siksaka09@users.noreply.github.com>
2022-02-14 20:50:24 +0700

    add ui

--- CUBIC WAR/CUBIC WAR/Action.cpp
+++ CUBIC WAR/CUBIC WAR/Action.cpp
@@ -1,8 +1,13 @@
 #include "Action.h"
 
-Action::Action(sf::RenderWindow* pWindow)
+Action::Action(sf::RenderWindow* pWindow, sf::Vector2f* pMousePosView)
 {
 	this->pWindow = pWindow;
+	this->pMousePosView = pMousePosView;
+	 this->intitText();
+	 this->initFont();
+	 this->initBar();
+	 this->initButton();
 }
 
 Action::~Action()
@@ -12,7 +17,7 @@ Action::~Action()
 
 void Action::StartGame(int numPlayer)
 {
-	this->pMap = new Map(this->pWindow, &this->currentMousePos, &this->idPlayerNow);
+	this->pMap = new Map;
 
 	for (int i = 0; i < numPlayer; i++) {
 		this->vPlayers.push_back(new Player);
@@ -30,12 +35,34 @@ void Action::GameEnd()
 	delete this->pMap;
 }
 
-void Action::RandomStartPosition()
+void Action::renderText(sf::RenderTarget *target)
 {
-	this->pMap->UpdatePlayerVision(this->pMap->vGrids[5][3].CreateBuilding("B", 0), 1,0);
-	this->pMap->UpdatePlayerVision(this->pMap->vGrids[6][3].AddUnit("Engineer", this->pMap->vUnits, 0), 1, 0);
-	this->pMap->UpdatePlayerVision(this->pMap->vGrids[8][12].CreateBuilding("B", 1), 1, 1);
-	this->pMap->UpdatePlayerVision(this->pMap->vGrids[7][13].AddUnit("Engineer", this->pMap->vUnits, 1), 1, 1);
+	target->draw(this->Playerturn);
+}
+
+void Action::Render()
+{
+	if (isWait == false) {
+		this->pMap->Render(this->pWindow);
+		ShowGridHighlight();
+		this->RenderUnit();
+		pWindow->draw(bar);
+		pWindow->draw(button);
+		pWindow->draw(rollbtn);
+		pWindow->draw(pointtext);
+		pWindow->draw(endturn);
+		pWindow->draw(rolltext);
+	}
+	else
+	{
+		this->updateText();
+		this->renderText(this->pWindow);
+		pWindow->draw(text);
+	}
+	
+	this->Update();
+	
+	
 }
 
 void Action::ClickEvents()
@@ -43,27 +70,46 @@ void Action::ClickEvents()
 	if (sf::Mouse::isButtonPressed(sf::Mouse::Left) || sf::Mouse::isButtonPressed(sf::Mouse::Right)) {
 		if (!this->isMousePress) {
 			// Mouse Left click
-			if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+			if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {  
 				if (this->isWait) {
 					this->isWait = false;
 				}
 				else {
 					if (pGridPointed != 0) {
-						if (gMode == "Move") {
+						if (this->isMoveMode) {
 							this->Move();
-							this->SetToNormalMode();
 						}
 						else {
 							if (pGridPointed->isUnit && pGridPointed->GetUnit() != 0) {
-								this->gMode = "Move";
 								this->pGridMoveStart = pGridPointed;
-								this->pMap->CreateGridArea(pGridMoveStart,1);
+								this->CreateGridArea(2);
+								this->isMoveMode = true;
 							}
 							else {
-
 							}
 						}
 					}
+				}
+			}
+			//endturn button click
+			if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+			{
+				if (button.getGlobalBounds().contains(sf::Mouse::getPosition(*pWindow).x, sf::Mouse::getPosition(*pWindow).y))
+				{	
+					std::cout << " => End Turn" << std::endl;
+					NextTurn();
+				}
+			}
+			//roll button click
+			if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+			{
+				if (rollbtn.getGlobalBounds().contains(sf::Mouse::getPosition(*pWindow).x, sf::Mouse::getPosition(*pWindow).y))
+				{
+					int ran1 = rand()%6+1;
+					int ran2 = rand() % 6 + 1;
+
+					point = point + ran1 + ran2;
+					
 				}
 			}
 			// Mouse Right click
@@ -71,6 +117,8 @@ void Action::ClickEvents()
 				std::cout << " => End Turn" << std::endl;
 				NextTurn();
 			}
+			
+			
 			this->isMousePress = true;
 		}
 	}
@@ -79,11 +127,13 @@ void Action::ClickEvents()
 	}
 }
 
-void Action::Move()
+void Action::initFont()
 {
-	this->pGridPointed->AddUnit(this->pGridMoveStart->GetUnit());
-	this->pMap->UpdatePlayerVision(pGridPointed, 1, this->idPlayerNow);
-	this->pGridMoveStart->ClearUnit();
+	this->font.loadFromFile("Fonts/Minecraft.ttf");
+	if (font.loadFromFile("Fonts/Minecraft.ttf"))
+	{
+		std::cout << "cantnot load font";
+	}
 }
 
 void Action::NextTurn()
@@ -100,31 +150,221 @@ void Action::NextTurn()
 		this->idPlayerNow = 0;
 		this->countTurn++;
 	}
-	this->SetToNormalMode();
+	
 	std::cout << "Turn: " << countTurn;
 	std::cout << " ----> Player " << idPlayerNow + 1 << " is playing";
+	//pWindow->draw(Playerturn);
 }
 
-void Action::SetToNormalMode()
+void Action::intitText()
 {
-	this->gMode = "Normal";
-	this->pGridMoveStart = 0;
-	this->pMap->SetGridAllEnable(true);
+	this->Playerturn.setFont(this->font);
+	this->Playerturn.setFillColor(sf::Color::White);
+	this->Playerturn.setCharacterSize(32);
+	this->Playerturn.setPosition(350.f, 280.f);
+
+	//Click to start text
+	this->text.setFont(this->font);
+	this->text.setFillColor(sf::Color::White);
+	this->text.setCharacterSize(18);
+	this->text.setPosition(680.f, 567.f);
+	this->text.setString("-> Click to Start ur turn");
+	//point text
+	this->pointtext.setFont(this->font);
+	this->pointtext.setFillColor(sf::Color::White);
+	this->pointtext.setCharacterSize(32);
+	this->pointtext.setPosition(5.f, 550.f);
+	//endtuentext
+	this->endturn.setFont(this->font);
+	this->endturn.setFillColor(sf::Color::White);
+	this->endturn.setCharacterSize(20);
+	this->endturn.setPosition(805.f, 560.f);
+	endturn.setString("End Turn");
+
+	this->rolltext.setFont(this->font);
+	this->rolltext.setFillColor(sf::Color::White);
+	this->rolltext.setCharacterSize(20);
+	this->rolltext.setPosition(705.f, 560.f);
+	rolltext.setString("Roll");
 }
+
 
 void Action::Update()
 {
+
+
 	this->currentMousePos = sf::Mouse::getPosition(*this->pWindow);
-	this->pGridPointed = this->pMap->UpdatePointedGrid();
+	this->pGridPointed = this->CheckGridPointed();
+
+	//button change color
+	if (button.getGlobalBounds().contains(sf::Mouse::getPosition(*pWindow).x, sf::Mouse::getPosition(*pWindow).y))
+	{
+		sf::Color lor(128, 128, 128);
+		button.setFillColor(lor);
+		rollbtn.setFillColor(lor);
+	}
+	else
+	{
+		sf::Color col(192, 192, 192);
+		button.setFillColor(col);
+		rollbtn.setFillColor(col);
+	}
+
+	if (rollbtn.getGlobalBounds().contains(sf::Mouse::getPosition(*pWindow).x, sf::Mouse::getPosition(*pWindow).y))
+	{
+		sf::Color lor(128, 128, 128);
+		rollbtn.setFillColor(lor);
+	}
+	else
+	{
+		sf::Color col(192, 192, 192);
+		rollbtn.setFillColor(col);
+	}
+	updateText();
 	ClickEvents();
 }
 
-void Action::Render()
+void Action::updateText()
 {
-	if (isWait == false) {
-		this->pMap->RenderMap();
-		this->pMap->ShowGridHighlight(this->gMode);
-		this->pMap->RenderUnits();
+	std::stringstream textt;
+
+	textt << "Player " << this->idPlayerNow+1 << " turn";
+	this->Playerturn.setString(textt.str());
+
+
+	//point update
+	std::stringstream pointp;
+
+	pointp << "Points : " << this->point;
+	this->pointtext.setString(pointp.str());
+}
+
+Grid* Action::CheckGridPointed()
+{
+	std::vector<Grid*> gridHovered;
+	for (unsigned int i = 0; i < this->pMap->vGrids.size(); i++) {
+		for (unsigned int j = 0; j < this->pMap->vGrids[i].size(); j++) {
+			if (pMap->vGrids[i][j].isHovered(currentMousePos))
+			{
+				gridHovered.push_back(&pMap->vGrids[i][j]);
+			}
+		}
 	}
-	this->Update();
+
+	if (gridHovered.size() > 0) {
+		for (unsigned int i = 0; i < gridHovered.size() - 1; i++) {
+			bool isTemp = 0;
+			for (unsigned int j = 0; j < gridHovered.size() - i - 1; j++) {
+				float distanceOne = gridHovered[j]->distanceFromMouse(this->currentMousePos);
+				float distanceTwo = gridHovered[j + 1]->distanceFromMouse(this->currentMousePos);
+				if (distanceOne > distanceTwo){
+					std::swap(gridHovered[j + 1], gridHovered[j]);
+					isTemp = 1;
+				}
+			}
+			if (!isTemp) break;
+		}
+		if(gridHovered[0]->isEnabled()) return gridHovered[0];
+	}
+	return 0;
+}
+
+void Action::CreateGridArea(int radius)
+{
+	this->pMap->SetGridAllEnable(false);
+	for (int q = this->pGridMoveStart->GetRC().y - radius; q <= this->pGridMoveStart->GetRC().y + radius; q++) {
+		for (int r = this->pGridMoveStart->GetRC().x - radius; r <= this->pGridMoveStart->GetRC().x + radius; r++) {
+			if (abs(-(q - this->pGridMoveStart->GetRC().y) - (r - this->pGridMoveStart->GetRC().x)) <= radius) {
+				int col = q + (r - (r & 1)) / 2;
+				int row = r;
+				if (row >= 0 && row < 11 && col >= 0 && col < 15) {
+					Grid* pGrid = &this->pMap->vGrids[row + 1][col + 1];
+					if (pGrid != this->pGridMoveStart && !pGrid->isBuilding && !pGrid->isUnit) {
+						pGrid->SetEnabled(true);
+					}
+				}
+			}
+		}
+	}
+	this->pMap->SetGridEdgeDisable();
+}
+
+void Action::HighlightGridPointed(sf::Color cFill, sf::Color cOutline)
+{
+	sf::CircleShape rect(35, 6);
+	rect.setPosition(pGridPointed->GetPosition().x - 5, pGridPointed->GetPosition().y);
+	rect.setFillColor(cFill);
+	rect.setOutlineColor(cOutline);
+	rect.setOutlineThickness(-1.5f);
+	this->pWindow->draw(rect);
+}
+
+void Action::HighlightGridArea(sf::Color cFill, sf::Color cOutline)
+{
+	sf::CircleShape rect(35, 6);
+	rect.setFillColor(cFill);
+	for (unsigned int i = 0; i < this->pMap->vGrids.size(); i++) {
+		for (unsigned int j = 0; j < this->pMap->vGrids[i].size(); j++) {
+			if (this->pMap->vGrids[i][j].isEnabled())
+			{
+				rect.setPosition(this->pMap->vGrids[i][j].GetPosition().x - 5, this->pMap->vGrids[i][j].GetPosition().y);
+				this->pWindow->draw(rect);
+			}
+		}
+	}
+
+}
+
+void Action::initBar()
+{
+	sf::Color temp(196, 164, 132);
+	this->bar.setFillColor(temp);
+	this->bar.setSize(sf::Vector2f(900.f, 100.f));
+	this->bar.setPosition(0.f, 550.f);
+}
+
+void Action::initButton()
+{
+	sf::Color col(192, 192, 192);
+	this->button.setFillColor(col);
+	this->button.setSize(sf::Vector2f(100.f, 50.f));
+	this->button.setPosition(800.f, 550.f);
+
+	this->rollbtn.setFillColor(col);
+	this->rollbtn.setSize(sf::Vector2f(50.f, 50.f));
+	this->rollbtn.setPosition(700.f, 550.f);
+}
+
+void Action::ShowGridHighlight()
+{
+	if (this->isMoveMode) {
+		this->HighlightGridArea(sf::Color(0, 255, 0, 100), sf::Color(0, 0, 0, 0));
+		if (this->pGridPointed != 0) this->HighlightGridPointed(sf::Color(0, 255, 0, 100), sf::Color(0, 0, 0, 0));
+	}
+	else {
+		if (this->pGridPointed != 0) this->HighlightGridPointed(sf::Color(0, 0, 0, 0), sf::Color::White);
+	}
+}
+
+void Action::RandomStartPosition()
+{
+	this->pMap->vGrids[5][3].CreateBuilding("B", 0);
+	this->pMap->vGrids[6][3].AddUnit("Engineer", this->pMap->vUnits, 0);
+	this->pMap->vGrids[8][12].CreateBuilding("B", 1);
+	this->pMap->vGrids[7][13].AddUnit("Engineer", this->pMap->vUnits, 1);
+}
+
+void Action::RenderUnit()
+{
+	for (unsigned int i = 0; i < this->pMap->vUnits.size(); i++) {
+		this->pMap->vUnits[i]->Render(this->pWindow);
+	}
+}
+
+void Action::Move()
+{
+	this->pGridPointed->AddUnit(this->pGridMoveStart->GetUnit());
+	this->pGridMoveStart->ClearUnit();
+	this->pGridMoveStart = 0;
+	this->isMoveMode = false;
 }
--- CUBIC WAR/CUBIC WAR/Action.h
+++ CUBIC WAR/CUBIC WAR/Action.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <sstream>
 
 #include "Map.h"
 #include "Player.h"
@@ -13,35 +14,66 @@ class Action
 {
 private:
 	void Update();
+	void updateText();
 
 	sf::RenderWindow* pWindow;
+
+	sf::Font font;
+	sf::Text Playerturn;
+	sf::Text text;
+	sf::Text pointtext;
+	sf::Text endturn;
+	sf::Text rolltext;
+
+	sf::RectangleShape bar;
+	sf::RectangleShape button;
+	sf::RectangleShape rollbtn;
 
 	Map* pMap = 0;
 
 	int countTurn = 0;
-	bool isWait = true;
-	std::vector<Player*> vPlayers;
-	int idPlayerNow = 0;
+	int point = 0;
 
+	int idPlayerNow = 0;
+	std::vector<Player*> vPlayers;
+	
+	std::vector<sf::Sprite> grids;
+
+	sf::Vector2f* pMousePosView;
 	sf::Vector2i currentMousePos;
 	bool isMousePress = false;
 
-	std::string gMode = "Normal";
-	void SetToNormalMode();
+	bool isWait = false;
+	bool isMoveMode = false;
+	bool isCreatdMode = false;
 
 	Grid* pGridMoveStart = 0;
 	Grid* pGridPointed = 0;
 
+	Grid* CheckGridPointed();
+	void CreateGridArea(int);
+
+	void initFont();
+	void intitText();
+	void initBar();
+	void initButton();
+
 	void ClickEvents();
 	void NextTurn();
+
+	void HighlightGridPointed(sf::Color, sf::Color);
+	void HighlightGridArea(sf::Color, sf::Color);
+	void ShowGridHighlight();
 	void RandomStartPosition();
 
+	void RenderUnit();
 	void Move();
 	
 public:
-	Action(sf::RenderWindow*);
+	Action(sf::RenderWindow*, sf::Vector2f*);
 	~Action();
 
+	void renderText(sf::RenderTarget*);
 	void Render();
 	void StartGame(int);
 	void GameEnd();
